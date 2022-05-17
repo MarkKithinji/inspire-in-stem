eceabfb06fd808ed8ad0eefef8ecac96e846f5c5
MarkKithinji <105651925+MarkKithinji@users.noreply.github.com>
2022-05-17 22:39:47 +0300

    python lesson-4.py

---
+++ lesson-4.py
@@ -0,0 +1,33 @@
+# integers
+
+x = 100
+y = 200
+
+sum_of = x + y
+diff = y - x
+prod = x * y
+quot = y / x
+
+print (" the sum of x and y is " +str(sum_of)) 
+print (" the diference of x and y is " +str(diff)) 
+print (" the product of x and y is " +str(prod)) 
+print (" the quotient of x and y is " +str(quot)) 
+
+# calculate the volume of a circle
+# surface area of a cylinder
+# area of a cirle
+# volume of a cylinder
+
+# let user type input
+
+radius = 21
+PI = 3.14
+
+area = PI * radius * radius
+print("area of circle is  " +str(area))
+
+height = 6
+volume_cyl = str(area) * 6
+
+print("volume of cylinder is "+str(volume_cyl))
+
